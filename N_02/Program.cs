8c7da0c54c90a0dd13f9ff136730876011b9afac
musicboy17 <pratham@yandex.ru>
2022-08-08 19:37:44 +0300

    All

---
+++ N_02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+if (N %2 ==0)
+{
+    Console.Write("Введённое число " + N + " чётное!");
+}
+else 
+{
+    Console.Write("Введённое число " + N + " НЕчётное!");
+}
---
+++ N_03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и 
+// выдаёт, какое число большее, а какое меньшее.
+
+
+Console.Write("Введите первое число N1: ");
+int number1=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число N2: ");
+int number2=Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number2 > max) 
+{
+    max = number2;
+}
+Console.WriteLine("Большее число " + max);
